d38ef786eb56df704a667d8cf8eba4d26988ae76
Churchill Nnanna <68286428+chapter-codes@users.noreply.github.com>
2023-11-06 01:30:42 +0100

    optimized the hero image for faster load time

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], 
+  theme: {
+    extend: {},
+    fontFamily:{
+      'inter':['Inter', "sans-serif"],
+      'playpen':['Playpen Sans', 'cursive'],
+      'rubik':['Rubik', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
+// font-family: 'Inter', sans-serif;
+// font-family: , cursive;
+// font-family: 'Rubik', sans-serif;
